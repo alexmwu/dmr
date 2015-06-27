7dd89e7245b6585cff55105043b2286ea8d315d5
Alex Wu <alexmwuwork@gmail.com>
2015-06-27 16:59:25 -0500

    fix server message

--- server/server.js
+++ server/server.js
@@ -4,8 +4,9 @@ var WebSocketServer = require('ws').Server,
 wss.on('connection', function connection(ws) {
   ws.on('message',function incoming(message) {
     console.log('received: %s', message);
+    ws.send(reverse(message));
   });
-  ws.send(reverse(message));
+  ws.send("Hello WebSocket!");
 });
 
 function reverse(s) {
