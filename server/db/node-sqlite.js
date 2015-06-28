06470305d82610f88c565fe4908dcddb20b21835
Alex Wu <alexmwuwork@gmail.com>
2015-06-28 08:48:55 -0500

    add new table definitions

--- server/db/node-sqlite.js
+++ server/db/node-sqlite.js
@@ -7,20 +7,30 @@ var db = new sqlite3.Database(file);
 
 db.serialize(function() {
   if(!exists) {
-    db.run("create table data ( \ 
+    db.run("create table bigdata ( \ 
       id int primary key not null, \
-      data text not null, \
+      data text \
     )");
     db.run("create table chunks ( \
-
+      id int primary key not null, \
+      foreign key(data_id) references bigdata(id) not null, \
+      chunk text text, \
+      chunk_index int not null \
     )");
     db.run("create table mappers ( \
-
+      id int primary key not null, \
+      
     )");
     db.run("create table reducers ( \
 
     )");
     db.run("create table results ( \
+
+    )");
+    db.run("create table jobs ( \
+
+    )");
+    db.run("create table chunk_jobs ( \
 
     )");
 });
