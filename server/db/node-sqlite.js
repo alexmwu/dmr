925d61bb96fe6a24261fe4a2ac78a933eb782610
Alex Wu <alexmwuwork@gmail.com>
2015-06-28 07:44:34 -0500

    update gitignore with oauth key file and txt files; change wsServer port so no clash

--- server/db/node-sqlite.js
+++ server/db/node-sqlite.js
@@ -1,2 +1,26 @@
+var fs = require("fs");
+var file = "app.db";
+var exists = fs.existsSync(file);
+
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('
+var db = new sqlite3.Database(file);
+
+db.serialize(function() {
+  if(!exists) {
+    db.run("create table data ( \ 
+      id int primary key not null, \
+      data text not null, \
+    )");
+    db.run("create table chunks ( \
+
+    )");
+    db.run("create table mappers ( \
+
+    )");
+    db.run("create table reducers ( \
+
+    )");
+    db.run("create table results ( \
+
+    )");
+});
